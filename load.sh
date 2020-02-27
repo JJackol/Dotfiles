92d876882bd35c04608f169d7833f6161992dad7
JJackol <jacekczerwonka@gmail.com>
2020-02-28 00:41:40 +0100

    load script

--- load.sh
+++ load.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 echo '[ -f ~/dotfiles/.zsh ] && source ~/dotfiles/.zsh ' >> ~/.zshrc
+echo '[ -f ~/dotfiles/.zsh ] && source ~/dotfiles/.zsh ' >> ~/.zshrc
+
 echo '[ -f ~/dotfiles/.profile ] && source ~/dotfiles/.profile ' >> ~/.profile
-echo ' source ~/dotfiles/.nvimrc ' >> ~/.config/nvim/init.vim
 echo '[ -f ~/dotfiles/.aliases ] && source ~/dotfiles/.aliases ' >> ~/.aliases
+
+echo 'source ~/dotfiles/.nvimrc ' >> ~/.config/nvim/init.vim
+echo 'source ~/dotfiles/.vimrc ' >> ~/.vimrc
+
 #echo "source ~/dotfiles/.zsh" >> ~/.zshrc
 #echo "source ~/dotfiles/.nvimrc" >> ~/.config/nvim/init.vim
 #echo "source ~/dotfiles/.profile" >> ~/.profile
